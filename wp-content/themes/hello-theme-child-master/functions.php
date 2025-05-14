b83e646c9d35a35d8f988e52d6c861fcce48d0d2
paul-le <55872209+paul-le@users.noreply.github.com>
2025-05-14 16:32:06 +0200

    Fix du bouton admin et petit bugs
    Fix du bouton admin et petit bugs comme les feuilles derrière la canette , peut etre recheck le formulaire dans la partie commande

--- wp-content/themes/hello-theme-child-master/functions.php
+++ wp-content/themes/hello-theme-child-master/functions.php
@@ -36,41 +36,38 @@ function hello_elementor_child_scripts_styles() {
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
 
 /**
- * Add admin button
+ * BOUTON ADMIN
  */
 
- function add_admin_link( $items , $args) {
-	if(is_user_logged_in()) {
-		$items .= '<li><a href="'. get_admin_url() .'">Admin</a></li>';
-	}
-	return $items;
-}
-
-add_filter( 'wp_nav_menu_items', 'add_admin_link', 10, 2);
-
-
-// add_filter( 'wp_nav_menu_items','add_admin_link', 10, 2 );
-
- 
-
-// function add_admin_link( $items, $args ) {
-
- 
-
-// if (is_user_logged_in() && $args->theme_location == 'menu-1') {
-
- 
-
-// $items .= '<li><a href="'. get_admin_url() .'">Admin</a></li>';
-
- 
-
+//  function add_admin_link( $items , $args) {
+// 	if(is_user_logged_in()) {
+// 		$items .= '<li><a href="'. get_admin_url() .'">Admin</a></li>';
+// 	}
+// 	return $items;
 // }
 
- 
+// add_filter( 'wp_nav_menu_items', 'add_admin_link', 10, 2);
 
-// return $items;
 
- 
+add_filter('wp_nav_menu_items', 'add_admin_link_elementor', 10, 2);
 
+function add_admin_link_elementor($items, $args) {
+    if (is_user_logged_in() && current_user_can('administrator')) {
+        $items .= '<li class="menu-item admin-link"><a href="' . esc_url(admin_url()) . '">Admin</a></li>';
+    }
+    return $items;
+}
+
+
+// add_filter('wp_nav_menu_items', 'add_admin_link_to_menu', 10, 2);
+
+// function add_admin_link_to_menu($items, $args) {
+//     if (is_user_logged_in()) {
+//         if ($args->theme_location == 'primary') {
+//             $admin_link = '<li class="menu-item admin-link"><a href="' . get_admin_url() . '">Admin</a></li>';
+//             $items .= $admin_link;
+//         }
+//     }
+
+//     return $items;
 // }
